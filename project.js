9443164e7b5d884f19c105feb1783c85dc91c615
Heath JH Moore <heathjhmoore@gmail.com>
2019-03-23 10:43:33 -0500

    added sort, clear, and count functions -- finished ticket

--- project.js
+++ project.js
@@ -1,1 +1,55 @@
 console.log("whats up");
+
+const printToDom = (divId, elementToAppend) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.appendChild(elementToAppend);
+}
+
+let count = 0
+
+const counter = () => {
+    count = count + 1;
+     console.log(count);
+}
+
+
+const sort = () => {
+    let domString = ``;
+    const studentName = document.getElementById("nameInput").value;
+    const houseNumber = Math.ceil(Math.random() * 4);
+    let studentHouse = ``;
+    if (houseNumber === 1) {
+        studentHouse = `Gryffindor`;
+    } else if (houseNumber === 2) {
+        studentHouse = `Hufflepuff`;
+    } else if (houseNumber === 3) {
+        studentHouse = `Ravenclaw`;
+    } else if (houseNumber === 4) {
+        studentHouse = `Slytherin`;
+    };
+    domString +=  `<h2>${studentName}<h2>`;
+    domString +=  `<p>${studentHouse}</p>`;
+    domString +=  `<button class="btn btn-primary expel" id="expel${count}">Expel</button>`;
+    const newCard = document.createElement('div');
+    newCard.innerHTML = domString;
+    newCard.id = `card${count}`;
+    newCard.className = `card`;
+    printToDom('cardContainer', newCard);
+}
+
+const clear = () => {
+    document.getElementById("nameInput").value = ``;
+}
+
+
+const buttonEvents = () => {
+    document.getElementById("sortButton").addEventListener('click', counter);
+    document.getElementById("sortButton").addEventListener('click', sort);
+    document.getElementById("sortButton").addEventListener("click", clear);
+}
+
+const init = () => {
+    buttonEvents();
+}
+
+init();
